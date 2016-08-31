eb93433e3df97d257c2b69780645610c7dce2ce2
gitQiang <h510493094@qq.com>
2016-08-31 10:31:06 +0800

    Update: 0831-correlation by grouped and rate by mean or median

--- IndexModels.R
+++ IndexModels.R
@@ -1,20 +1,39 @@
-source("//BJB-327/IndexModels/misc.R")
-datapath <- "E:/HQ/IndexModels/价格指标Wind"
+rm(list=ls())
+gc()
+
+#source("//BJB-327/IndexModels/misc.R")
+#datapath <- "E:/HQ/IndexModels/价格指标Wind"
+
+source("misc.R")
+datapath <- "//BJB-155/IndexModels/价格指标Wind"
 
 
 ##### the initial Index model
 Indexs <- read.delim(paste(datapath,"/IDs_LABs.txt",sep=""),sep="\t")
-day <- as.character(Sys.Date())
+day <- '2016-08-26' ##as.character(Sys.Date())
 rawData <- read.csv(paste(datapath,"/WindPriceIndex_",day,".csv",sep=""))
 rawData <- as.matrix(rawData)
 
-newData <- fillWeek(Indexs,rawData)
-dimL <- c("day","week","month","season")
 
+#weekI <- which(Indexs[,7]=="周")
+kv <- sapply(2:ncol(rawData), function(i) sum(is.na(rawData[,i]))/sum(!is.na(rawData[,i])))
+weekI <- which(kv>=4)
+## newData <- fillWeek(Indexs,rawData) # method 1: not suitable for week data
+## newData <- rawData # method 2
+subs <- curveOne(rawData,Indexs,weekI) ## method 3
+newData <- rawData[,c(1,subs+1)]
+Indexs <- Indexs[subs, ]
+
+
+kv <- sapply(2:ncol(newData), function(i) sum(is.na(newData[,i]))/sum(!is.na(newData[,i])))
+weekI <- which(kv>=4)
+dimL <- c("day","week","month","season")
 for(i in 1:length(dimL)){
-        rates <- rateOne(newData,dimL[i])
+        rates <- rateOne(newData,weekI,dimL[i])
         r1 <- IndexModel_1(Indexs,rates)
         print(r1)
 }
 
+r0 <- pta_rates()
+print(r0)
 
--- misc.R
+++ misc.R
@@ -1,10 +1,22 @@
-rateOne <- function(newData,dim="day"){
+rateOne <- function(newData,weekI,dim="day"){
         L <- c(1,7,30,90)
+        Lweek <- c(1,1,4,12)
         ds <- c("day","week","month","season")
         Lone <- L[ds==dim]
+        Ltwo <- Lweek[ds==dim]
         
-        sapply(2:ncol(newData), function(i){
-                #print(i)
+        rates <- rep(0,ncol(newData)-1)
+        subs2 <- weekI+1
+        subs1 <- setdiff(2:ncol(newData), subs2)
+        
+        rates[subs1-1] <- rate0(newData,Lone,subs1)
+        rates[subs2-1] <- rate0(newData,Ltwo,subs2)
+        
+        rates
+}
+
+rate0 <- function(newData,Lone,subs){
+        sapply(subs, function(i){
                 x <- as.numeric(newData[,i])
                 subs <- which(!is.na(x))
                 n <- length(subs)
@@ -17,8 +29,7 @@ rateOne <- function(newData,dim="day"){
                 }else{
                         ( median(x[seq1]) - median(x[seq2]) )/( median(x[seq2]) )
                 }
-        })
-
+        })    
 }
 
 IndexModel_1 <- function(Indexs,rates){
@@ -45,3 +56,113 @@ fillWeek <- function(Indexs,rawData){
         
         newData
 }
+
+curveOne <- function(newData, Indexs,weekI){
+        
+        ptaP <- pta_pricef()
+        ptaweek <- as.matrix(ptaP[,2,drop=FALSE])
+        rownames(ptaweek) <- ptaP[,1]
+        mode(ptaweek) <- "numeric"
+        weekp <- groupPredict(ptaweek)  
+        
+        subs2 <- weekI+1
+        subs1 <- setdiff(2:ncol(newData), subs2)
+        corV <- rep(-2,ncol(newData)-1)
+        
+        Lone=90
+        interD <- intersect(newData[,1],ptaP[,1])
+        y <- as.numeric(ptaP[ptaP[,1] %in% interD,2])
+        corV[subs1-1] <- sapply(subs1, function(i){
+                #print(i)
+                x <- as.numeric(newData[newData[,1] %in% interD,i])
+                subs <- which(!is.na(x) & !is.na(y))
+                n <- length(subs)
+                
+                seq1 <- subs[max(0,n-2*Lone+1):n]
+                cor(x[seq1],y[seq1])
+        })
+        
+        Lone=12
+        corV[subs2-1] <- sapply(subs2, function(i){
+                #print(i)
+                tmp <- !is.na(newData[,i])
+                x0 <- newData[tmp,i,drop=FALSE]
+                mode(x0) <- "numeric"
+                rownames(x0) <- paste(year(newData[tmp,1]),week(newData[tmp,1]),sep="-")
+                
+                interD <- intersect(rownames(weekp),rownames(x0))
+                x <- x0[interD, 1]
+                y <- weekp[interD, 1]
+                
+                subs <- which(!is.na(x) & !is.na(y))
+                n <- length(subs)
+                
+                seq1 <- subs[max(0,n-2*Lone+1):n]
+                cor(x[seq1],y[seq1])
+        })
+        
+        corV[is.na(corV)] <- 0
+        #plot(corV)
+        ## random 1000 cor 3*sd ~~ 0.2
+        subs <- which(corV>0.2)
+        
+        subs
+}
+
+groupDate <- function(date1){
+        library(lubridate)
+        useDates <- as.Date(date1)
+        
+        w1 <- as.numeric(week(useDates))
+        w1[w1<10] <- paste(0,w1[w1<10],sep="")
+        m1 <- as.numeric(month(useDates))
+        m1[m1<10] <- paste(0,m1[m1<10],sep="")
+        
+        gs1 <- paste(year(useDates),w1,sep="-")
+        gs2 <- paste(year(useDates),m1,sep="-")
+        gs3 <- paste(year(useDates),quarter(useDates),sep="-")
+        
+        useDates <- cbind(useDates,gs1,gs2,gs3)
+        useDates
+}
+
+pta_pricef <- function(){
+
+        options(stringsAsFactors = FALSE)
+        #ptaP <- read.delim("E:/HQ/IndexModels/data/PTA华东市场价格.txt",sep="\t")
+        ptaP <- read.delim("//BJB-155/IndexModels/data/PTA华东市场价格.txt",sep="\t")
+        ptaP <- ptaP[-1, ]
+        ptaP[,2] <- gsub(",","",ptaP[,2])
+        ptaP[,2] <- gsub(" ","",ptaP[,2])
+        ptaP <- ptaP[!grepl("数据来源",ptaP[,1]), ]
+        ptaP <- ptaP[ptaP[,1]!="", ]
+        ptaP <- ptaP[!is.na(as.numeric(ptaP[,2])), ]
+        ptaP <- as.matrix(ptaP)
+        
+        ptaP
+}
+
+pta_rates <- function(){
+        
+        ptaP <- pta_pricef()
+        gDate <- groupDate(ptaP[,1])
+        x <- as.numeric(ptaP[,2])
+        
+        L=4
+        rone <- matrix(0,4,L)
+        for(i in 1:L){
+                onet <- gDate[,i]
+                seq1 <- which(onet==max(onet))
+                seq2 <- which( onet==max(onet[onet!=max(onet)]) )
+                
+                rone[1,i] <- ( min(x[seq1]) - min(x[seq2]) )/( min(x[seq2]) )
+                rone[2,i] <- ( mean(x[seq1]) - mean(x[seq2]) )/( mean(x[seq2]) )
+                rone[3,i] <- ( median(x[seq1]) - median(x[seq2]) )/( median(x[seq2]) )
+                rone[4,i] <- ( max(x[seq1]) - max(x[seq2]) )/( max(x[seq2]) )
+        }
+        colnames(rone) <- c("day","week","month","season")
+        rownames(rone) <- c("min","mean","median","max")
+        
+        rone
+}
+
